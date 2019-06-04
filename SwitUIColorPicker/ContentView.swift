c12be9728a62ab9e313cb76a2ed7974e992beac7
Alex Chase <alex@joymode.com>
2019-06-03 23:54:12 -0700

    Initial Commit

---
+++ SwitUIColorPicker/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwitUIColorPicker
+//
+//  Created by Alex Chase on 6/3/19.
+//  Copyright © 2019 AlexChase. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
